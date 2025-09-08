fab4731686e2528e99c63e7b075eda4d9c6fe51d
edimurg <murg.mihai.mm@gmail.com>
2025-09-08 16:43:39 +0300

    Update server_stats.sh

--- server_stats.sh
+++ server_stats.sh
@@ -60,8 +60,8 @@ memory(){   #this is the memory function
 total_memory=`free -h | tr -d 'Gi' | awk '{print $2}' | sed '2!d'`
         echo " "
         used_memory=`free -h | tr -d 'Gi' | awk '{print $3}' | sed '2!d'`
-	math=`echo "$used_memory * 100 / $total_memory / 2 " | bc`    #this will give us the usage procentage and will scale it to the bar, for every 1% of usage we will print one block 
-	procentage=`echo "$math * 2 + 1" | bc `
+	math=`echo "$used_memory * 100 / $total_memory / 2 " | bc`    #this will give us the usage precentage and will scale it to the bar, for every 1% of usage we will print one block 
+	precentage=`echo "$math * 2 + 1" | bc `
 	echo -e "\033[4m\033[1mMemory	\033[00m\033[0;36m█\033[0m Total\033[0;31m █\033[0m Used \n"
 	echo -e "Total memory is $total_memory GB\n"
 	echo -e "\033[0;36m▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉" #there is 50 of this blocks
@@ -69,7 +69,7 @@ total_memory=`free -h | tr -d 'Gi' | awk '{print $2}' | sed '2!d'`
 	echo -e -n "\033[0;31m█\033[0m"
         
 done
-echo -e " $used_memory GB ($procentage%)                                                \n"
+echo -e " $used_memory GB ($precentage%)                                                \n"
 }
 
 cpu(){ 	total_cpu_usage=`top -bn1 | grep 'Cpu(s)' | awk '{print 100 - $8}'` #this function will monitor the cpu activity
@@ -86,7 +86,7 @@ cpu(){ 	total_cpu_usage=`top -bn1 | grep 'Cpu(s)' | awk '{print 100 - $8}'` #thi
 	echo "▇▇▇▇▇▇▇▇▇▇▇ 20%"
 	echo "▇▇▇▇▇▇ 10%"
 	echo "▇▇▇ 5%"
-	math=`echo "$total_cpu_usage / 2 + 1 " | bc`  #scalaing the procentage to the bar
+	math=`echo "$total_cpu_usage / 2 + 1 " | bc`  #scalaing the precentage to the bar
 	for ((i=0;i<$math;i++));do  #underneath bar
 		echo -e -n "\033[0;31m█\033[0m"
 	done
@@ -96,9 +96,9 @@ cpu(){ 	total_cpu_usage=`top -bn1 | grep 'Cpu(s)' | awk '{print 100 - $8}'` #thi
 storage() {   # this function will monitor the sorage
 	total_space=`df -h / | awk '{print $2}' | sed '2!d' | tr -d 'G'`
 	used_space=`df -h / | awk '{print $3}' | sed '2!d' | tr -d 'G'`
-	math=`echo "$used_space * 100 / $total_space / 2 " | bc`  # scaling the procentage to the bar
-	procentage=`echo "$math * 2 + 1" | bc`		
- #this is to get the real procentage
+	math=`echo "$used_space * 100 / $total_space / 2 " | bc`  # scaling the precentage to the bar
+	precentage=`echo "$math * 2 + 1" | bc`		
+ #this is to get the real precentage
 	echo " "
 	echo -e "\033[4m\033[1mStorage\033[0m   \033[00m\033[0;34m█\033[0m Total\033[0;31m █\033[0m Used \n"
 	echo -e "Total Disk Space is $total_space GB\n"
@@ -106,20 +106,20 @@ storage() {   # this function will monitor the sorage
 	for ((i=0;i<$math;i++));do  #underneath bar
 	echo -e -n "\033[0;31m█\033[0m"
 done
-echo -e " $used_space GB ($procentage%) \n                                     "
+echo -e " $used_space GB ($precentage%) \n                                     "
 }
 
 active_processes() {  # this function will monitor top 5 processes with the most cpu and memory needs
 	echo " "
-	procentage_cpu=(`echo -e "\033[4;33mCPU\033[0m" ; top -b | head -12 | awk '{print $9}' | sed '8,12!d'`)   #used cpu
-	procentage_ram=(`echo -e "\033[4;33mMEM\033[0m" ; top -b | head -12 | awk '{print $10}' | sed '8,12!d'`)  #used memory
+	precentage_cpu=(`echo -e "\033[4;33mCPU\033[0m" ; top -b | head -12 | awk '{print $9}' | sed '8,12!d'`)   #used cpu
+	precentage_ram=(`echo -e "\033[4;33mMEM\033[0m" ; top -b | head -12 | awk '{print $10}' | sed '8,12!d'`)  #used memory
 	processes=(`echo -e "\033[4;33mProcesses\033[0m" ;  top -b | head -12 | awk '{print $12}' | sed '8,12!d'`)  # the processes
 
 	for ((s=0;s<1;s++));do
-		echo "${procentage_ram[s]} ${procentage_cpu[s]} ${processes[s]}" # here we create 3 colums in paralel, memory usage, cpu usage, and the processes
+		echo "${precentage_ram[s]} ${precentage_cpu[s]} ${processes[s]}" # here we create 3 colums in paralel, memory usage, cpu usage, and the processes
 	done
 	for (( i=1;i<6;i++));do
-		echo "${procentage_ram[i]}% ${procentage_cpu[i]}% ${processes[i]}          	"
+		echo "${precentage_ram[i]}% ${precentage_cpu[i]}% ${processes[i]}          	"
 	done
 	echo " "
  } 
